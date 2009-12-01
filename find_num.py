54d920a7c86189f368bc574ff80fb3b617a43b29
Jon Manese <jon@itachi.(none)>
2009-12-01 16:07:37 +0800

    raffle query somewhat working

--- find_num.py
+++ find_num.py
@@ -5,18 +5,21 @@ import sys
 
 def find_num(con, num):
     if not type(num) == str:
-        raise 'This is crazy'
+        raise 'Parameter should be a string!'
+    if len(num) > 6:
+        num = num[0:6]
+    if len(num) == 0:
+        return []
     digits = len(num)
     cols = ','.join(['c' + str(n+1) for n in range(digits)])
     parm = ','.join(['?'] * digits)
     where_clause = ' and '.join(['c' + str(n+1) + '=?' for n in range(digits)])
-    params = [int(i) for i in num]
+    params = list(num)
     cu = con.cursor()
     cu.execute('select c1||c2||c3||c4||c5||c6 from numbers where %s' %
             where_clause, params)
     ret = cu.fetchall()
     cu.close()
-    print ret
     return ret
 
 if __name__ == '__main__':
--- raffle.py
+++ raffle.py
@@ -4,15 +4,29 @@ import os
 sys.path.append(os.path.join(sys.path[0], 'webpy'))
 
 import web
+import sqlite3
+import json
+from sqlalchemy.pool import SingletonThreadPool
+
+from find_num import find_num
 
 urls = ('/', 'Raffle',
+        '/query', 'Query',
         '/setup', 'Setup')
 
 app = web.application(urls, globals())
+render = web.template.render('templates')
+pool = SingletonThreadPool(lambda: sqlite3.connect('numbers.db'))
 
 class Raffle(object):
     def GET(self):
-        return 'hello fucking world'
+        return render.raffle()
+
+class Query(object):
+    def GET(self):
+        num = web.input(num='').num
+        result = find_num(pool.connect(), str(num))
+        return json.dumps({'status': status, 'results':[r[0] for r in result]})
 
 class Setup(object):
     def GET(self):
